abb6e92095cfb57715d6b37ea980610bbd59273a
Hausberger János <hausberger.janos@codeyard.eu>
2023-12-03 17:32:14 +0100

    Handling Duplicate Database Fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,13 @@
 const AppError = require('../utils/appError');
 
-// eslint-disable-next-line prettier/prettier
 const handleDBCastError = (err) => {
   const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDBDuplicatedFields = (err) => {
+  const value = err.keyValue.name;
+  const message = `Duplicate field value: "${value}". Please use another value`;
   return new AppError(message, 400);
 };
 
@@ -46,6 +51,7 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
 
     if (err.name === 'CastError') error = handleDBCastError(error);
+    if (err.code === 11000) error = handleDBDuplicatedFields(error);
 
     sendErrorProd(error, res);
   }
