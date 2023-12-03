38afc1b5e9b01139834fdb2250abbecfbab34416
Hausberger János <hausberger.janos@codeyard.eu>
2023-12-03 12:34:03 +0100

    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -14,11 +14,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware!👋');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -26,5 +21,12 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl}`,
+  });
+});
 
 module.exports = app;
