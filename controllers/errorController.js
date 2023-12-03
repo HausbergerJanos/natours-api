38a00bbdedeb01b68c4a8c322c217081df5674e5
Hausberger János <hausberger.janos@codeyard.eu>
2023-12-03 17:18:53 +0100

    Handling Invalid Database IDs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,11 @@
+const AppError = require('../utils/appError');
+
+// eslint-disable-next-line prettier/prettier
+const handleDBCastError = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -29,14 +37,16 @@ const sendErrorProd = (err, res) => {
 };
 
 module.exports = (err, req, res, next) => {
-  //console.log(err.stack);
-
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+
+    if (err.name === 'CastError') error = handleDBCastError(error);
+
+    sendErrorProd(error, res);
   }
 };
