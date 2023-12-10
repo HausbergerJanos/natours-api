2c8a225620ee1d39118cb8621119acc803064024
Hausberger János <hausberger.janos@codeyard.eu>
2023-12-10 15:54:37 +0100

    Nested Routes with Express

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
+// POST /tours/sdsm4sd4/reviews
+// POST /reviews
 router
   .route('/')
   .get(authController.protect, reviewController.getAllReviews)
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
-const reviewController = require('../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkId);
+
+// POST /tours/sdsm4sd4/reviews
+// GET /tours/sdsm4sd4/reviews
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
@@ -27,17 +31,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour,
-  );
-
-// POST /tours/sdsm4sd4/reviews
-// GET /tours/sdsm4sd4/reviews
-// GET /tours/sdsm4sd4/reviews/dsd4464sd
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview,
   );
 
 module.exports = router;
