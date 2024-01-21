4b1803533ab96b872167ca693cb1523f7117ef17
Hausberger János <hausberger.janos@codeyard.eu>
2024-01-21 15:21:04 +0100

    Building the Tour Page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,8 +13,17 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) Get the data for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    select: 'review rating -tour',
+  });
+
+  // 2) Build template
+  // 3) Render template
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   });
-};
+});
